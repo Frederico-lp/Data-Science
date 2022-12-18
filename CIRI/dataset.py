d9c6c4481537901f2a016f4dde7a8b2333c2473d
kjwdamme <kjwdamme@avans.nl>
2022-12-18 16:39:16 +0100

    Improved loading the dataset so that it does not completely get loaded into memory

--- CIRI/dataset.py
+++ CIRI/dataset.py
@@ -1,7 +1,6 @@
 from torch.utils.data import Dataset
 import numpy as np
 import os
-import matplotlib.image as mpimg
 from PIL import Image
 
 
@@ -10,30 +9,41 @@ class IncidentsDataset(Dataset):
         self.target_transform = target_transform
         self.transform = transform
         self.labels = np.array(next(os.walk(img_dir))[1])
-        self.data, self.targets = self.__load_data(img_dir)
+        # self.data, self.targets = self.__load_data(img_dir)
+        self.image_paths, self.targets = self.__get_paths_and_targets(img_dir)
 
     def __len__(self):
-        return len(self.data)
+        return len(self.targets)
 
     def __getitem__(self, index):
-        return self.data[index]
+        path = self.image_paths[index]
+        img = Image.open(path).convert('RGB')
 
-    def __load_data(self, directory):
-        print("test")
-        images, targets = [], []
+        if self.transform:
+            img = self.transform(img)
 
-        for target in np.arange(len(self.labels)):
-            path = directory + "/" + self.labels[target]
-            file_names = os.listdir(path=path)
+        return img, self.targets[index]
 
-            for filepath in file_names:
-                if (filepath.endswith(".jpg") or filepath.endswith(".jpeg")) and not filepath.startswith("."):
-                    img = Image.open(path + "/" + filepath).convert('RGB')
-                    # img = mpimg.imread(path + "/" + filepath)
-                    if self.transform:
-                        img = self.transform(img)
-                    images.append(img)
-                    targets.append(target)
+    def get_item_with_target(self, target, index):
+        label = self.labels[target]
 
-        return np.array(images), np.array(targets)
+        path = self.image_paths[[label in x for x in self.image_paths]][index]
+        img = Image.open(path).convert('RGB')
 
+        if self.transform:
+            img = self.transform(img)
+
+        return img
+
+    def __get_paths_and_targets(self, img_dir):
+        paths, targets = [], []
+
+        for t in np.arange(len(self.labels)):
+            path = img_dir + "/" + self.labels[t]
+            file_names = os.listdir(path)
+            for name in file_names:
+                if (name.endswith(".jpg") or name.endswith(".jpeg")) and not name.startswith("."):
+                    paths.append(path + "/" + name)
+                    targets.append(t)
+
+        return np.array(paths), np.array(targets)
